2db12993ee49a1976fe6c539388be803c65b8044
saidone <saidone@saidone.org>
2019-05-09 09:45:47 +0200

    Hide *nrepl-connection* and *nrepl-server* from switch-to-buffer list

--- lisp/user-init-default-modes.el
+++ lisp/user-init-default-modes.el
@@ -10,5 +10,7 @@
 
 ;; don't show REPL when first connected
 (setq cider-repl-pop-to-buffer-on-connect nil)
+;; hide *nrepl-connection* and *nrepl-server* from switch-to-buffer list
+(setq nrepl-hide-special-buffers t)
 
 (provide 'user-init-default-modes)

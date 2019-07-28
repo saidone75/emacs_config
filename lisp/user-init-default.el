0951311853d8237c9656458785d3d134b906cdc2
saidone <saidone@saidone.org>
2019-07-28 08:27:02 +0200

    Disable scroll bar, removed ido-mode unused code

--- lisp/user-init-default.el
+++ lisp/user-init-default.el
@@ -6,8 +6,8 @@
   (tool-bar-mode 0))
 
 ;; disable scrollbar
-;(when (fboundp 'set-scroll-bar-mode)
-;  (set-scroll-bar-mode nil))
+(when (fboundp 'set-scroll-bar-mode)
+  (set-scroll-bar-mode nil))
 
 ;; enable narrowing
 (put 'narrow-to-region 'disabled nil)
@@ -27,11 +27,6 @@
 
 ;; disable the splash screen
 (setq inhibit-splash-screen t)
-
-;; enable ido mode
-;(require 'ido)
-;(ido-mode t)
-;(setq ido-enable-flex-matching t) ;; enable fuzzy matching
 
 ;; use spaces instead of tabs
 (setq-default indent-tabs-mode nil)

2175a1692025a014acb23a78b9dea78e5d73e303
saidone <saidone@saidone.org>
2019-07-28 09:29:45 +0200

    Add alias definition for quick start in bash

--- init_quick.el
+++ init_quick.el
@@ -1,3 +1,5 @@
+;; quick startup in terminal
+;; alias em='emacs -Q -nw --load ~/.emacs.d/init_quick.el'
 (let ((default-directory  "~/.emacs.d/elpa"))
   (normal-top-level-add-subdirs-to-load-path))
 (require 'zenburn-theme)

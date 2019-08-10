f701cd6a8f230a963049bce7cbf8e9701db3c141
saidone <saidone@saidone.org>
2019-08-10 07:45:07 +0200

    disable menu bar, lower GC threshold

--- init_quick.el
+++ init_quick.el
@@ -1,5 +1,14 @@
 ;; quick startup in terminal
 ;; alias em='emacs -Q -nw --load ~/.emacs.d/init_quick.el'
+
 (let ((default-directory  "~/.emacs.d/elpa"))
   (normal-top-level-add-subdirs-to-load-path))
+
+;; set theme
 (require 'zenburn-theme)
+
+;; disable menu bar
+(menu-bar-mode -1)
+
+;; set GC threshold to 16 MiB
+(setq gc-cons-threshold (expt 2 24))

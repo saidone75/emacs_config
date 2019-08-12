a20cdf387f4bc6933ea6a95db63041b3e00dac50
saidone <saidone@saidone.org>
2019-08-12 21:21:14 +0200

    remove duplicated key binding for projectile

--- lisp/user-init-projectile.el
+++ lisp/user-init-projectile.el
@@ -1,14 +1,12 @@
 (message "configuring projectile")
 
 (projectile-mode)
-(define-key projectile-mode-map (kbd "s-p") 'projectile-command-map)
+(define-key projectile-mode-map (kbd "C-c p") 'projectile-command-map)
 (setq projectile-mode-line
       '(:eval (format " Projectile[%s]"
                       (projectile-project-name))))
 
 ;; use ag instead of find as generic command
 (setq projectile-generic-command "ag -l0")
-
-;(add-hook 'after-init-hook #'projectile-global-mode)
 
 (provide 'user-init-projectile)

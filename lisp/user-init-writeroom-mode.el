59377baeb9b27325247af17b53c6fd2399ae28a9
saidone <saidone@saidone.org>
2019-08-15 13:59:45 +0200

    set keybinding for writeroom-mode

--- lisp/user-init-writeroom-mode.el
+++ lisp/user-init-writeroom-mode.el
@@ -2,6 +2,9 @@
 
 (with-eval-after-load 'writeroom-mode
 
+  ;; set automatic fullscreen behaviour
+  (define-writeroom-global-effect fullscreen 'fullboth)
+  
   ;; switch theme when entering writeroom-mode
   (defun my-writeroom-theme (arg)
     (cond
@@ -10,6 +13,8 @@
      ((= arg -1)
       (disable-theme 'solarized-light))))
 
-  (add-to-list 'writeroom-global-effects #'my-writeroom-theme))
+  (add-to-list 'writeroom-global-effects 'my-writeroom-theme))
+
+(global-set-key (kbd "C-x w w") 'writeroom-mode)
 
 (provide 'user-init-writeroom-mode)

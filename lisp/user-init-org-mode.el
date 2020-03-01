7428ec1a01fff221f3f7a14eb9027217e3bb9f6e
saidone <saidone@saidone.org>
2020-03-01 08:46:11 +0100

    org-mode default capture file, auto save schedule

--- lisp/user-init-org-mode.el
+++ lisp/user-init-org-mode.el
@@ -2,6 +2,13 @@
 
 (global-set-key (kbd "C-c l") 'org-store-link)
 (global-set-key (kbd "C-c a") 'org-agenda)
+(global-set-key (kbd "C-c c") 'org-capture)
+
+;; set default capture file
+(setq org-default-notes-file (concat org-directory "/refile.org"))
+
+;; save all org buffers every 1 minute before the hour
+(run-at-time "00:59" 3600 'org-save-all-org-buffers)
 
 ;; set maximum indentation for description lists
 (setq org-list-description-max-indent 5)

31203be2aabd25354030dce99e3204f8cc455305
saidone <saidone@saidone.org>
2020-03-17 11:54:42 +0100

    magit-kill-buffers

--- lisp/user-init-magit.el
+++ lisp/user-init-magit.el
@@ -1,5 +1,15 @@
 (message "configuring magit")
 
+(with-eval-after-load 'magit
+
+  (defun help/magit-kill-buffers ()
+    (interactive)
+    (let ((buffers (magit-mode-get-buffers)))
+      (magit-restore-window-configuration)
+      (mapc #'kill-buffer buffers)))
+
+  (define-key magit-status-mode-map (kbd "q") #'help/magit-kill-buffers))
+
 (global-set-key (kbd "C-x g") 'magit-status)
 
 (provide 'user-init-magit)

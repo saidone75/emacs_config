e4efb5467181924ff97450e30e270db12fde94ec
saidone <saidone@saidone.org>
2020-03-28 21:58:20 +0100

    view on browser

--- lisp/user-init-auctex.el
+++ lisp/user-init-auctex.el
@@ -1,3 +1,8 @@
 (message "configuring auctex")
 
+(setq TeX-PDF-mode t)
+
+(setq TeX-view-program-list '(("Chrome" "google-chrome %o")))
+(setq TeX-view-program-selection '((output-pdf "Chrome")))
+
 (provide 'user-init-auctex)

26bf6dea21360ba18cb9369480e09fb0aaeca0a3
saidone <saidone@saidone.org>
2020-03-29 10:11:44 +0200

    auto-save, flyspell and other config

--- lisp/user-init-auctex.el
+++ lisp/user-init-auctex.el
@@ -1,8 +1,20 @@
 (message "configuring auctex")
 
-(setq TeX-PDF-mode t)
+(setq TeX-save-query nil)
 
+(setq TeX-auto-save t)
+(setq TeX-parse-self t)
+(setq-default TeX-master nil)
+
+(setq TeX-PDF-mode t)
 (setq TeX-view-program-list '(("Chrome" "google-chrome %o")))
 (setq TeX-view-program-selection '((output-pdf "Chrome")))
+
+(add-hook 'LaTeX-mode-hook 'visual-line-mode)
+(add-hook 'LaTeX-mode-hook 'flyspell-mode)
+(add-hook 'LaTeX-mode-hook 'LaTeX-math-mode)
+
+(add-hook 'LaTeX-mode-hook 'turn-on-reftex)
+(setq reftex-plug-into-AUCTeX t)
 
 (provide 'user-init-auctex)

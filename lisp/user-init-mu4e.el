e719fd6d6bf6346d01634df7ab8fd0e4a8b1bec4
saidone <saidone@saidone.org>
2020-04-09 18:59:28 +0200

    back to the original background color on headers

--- lisp/user-init-mu4e.el
+++ lisp/user-init-mu4e.el
@@ -100,9 +100,9 @@
 (setq mu4e-headers-visible-lines 20)
 
 ;; highlight color
-(add-hook 'mu4e-headers-mode-hook
-          (lambda ()
-            (set-face-background 'mu4e-header-highlight-face "#4F4F4F")))
+;; (add-hook 'mu4e-headers-mode-hook
+;;           (lambda ()
+;;             (set-face-background 'mu4e-header-highlight-face "#4F4F4F")))
 
 (setq epa-pinentry-mode 'loopback)
 
